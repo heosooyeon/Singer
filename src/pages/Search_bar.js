23780510be3ef36b2070597f6934237ce87bf43b
heosooyeon <80025144+heosooyeon@users.noreply.github.com>
2023-06-01 16:45:31 +0900

    Api try other method

--- src/pages/Search_bar.js
+++ src/pages/Search_bar.js
@@ -2,21 +2,33 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Input() {
+  const [music, setMusic] = useState("");
+  const onChangeSearch = (e) => {
+    setMusic(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        "https://m.search.naver.com/p/csearch/content/qapirender.nhn?where=nexearch&key=LyricsSearchResult&q=가사검색 너를"
+      );
+      console.log(res.data);
+      return res.data;
+    };
+
+    fetchData().then((res) => setData(res));
+  }, []);
+
   return (
     <div>
       <input
         type="text"
         id="search"
-        onClick={() => {
-          axios
-            .get(
-              "https://m.search.naver.com/p/csearch/content/qapirender.nhn?where=nexearch&key" +
-                "=LyricsSearchResult&"
-            )
-            .then((result) => {
-              console.log(result.data);
-            });
-        }}
+        value={music}
+        onClick={onChangeSearch}
       ></input>
       <div id="result"></div>
     </div>
